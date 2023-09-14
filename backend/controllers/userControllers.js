7851fae25aa5b3298976404da87181d48d7613b2
irinamihai150 <irinamihai150@gmail.com>
2023-09-14 22:46:50 +0100

    finish endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
-
+import User from "../models/userModels.js"
+import generateToken from "../utils/generateToken.js"
 //description Auth user/set token
 //route POST api/users/auth
 //@access Public
@@ -11,9 +12,29 @@ const authUser = asyncHandler(async (req, res) => {
 //description register a new user
 //route POST api/users
 //@access Public
-
 const registerUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Register User" })
+	// console.log(req.body)
+	const { name, email, password } = req.body
+	const userExists = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error("User already exists")
+	}
+
+	const user = await User.create({ name, email, password })
+
+	if (user) {
+		generateToken(res, user._id)
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		})
+	} else {
+		res.status(400)
+		throw new Error("Invalid user data")
+	}
 })
 
 //description Logout user
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ import userRoutes from "./routes/userRoutes.js"
 connectDB()
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/api/users", userRoutes)
+
 app.get("/", (req, res) => {
 	res.send("Server is ready")
 })
