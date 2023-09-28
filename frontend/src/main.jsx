699c9a637a42b79077e528eb979685726682a9f2
irinamihai150 <irinamihai150@gmail.com>
2023-09-28 22:01:50 +0100

    attempt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,34 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
 import {
 	createBrowserRouter,
 	createRoutesFromElements,
 	Route,
 	RouterProvider,
 } from "react-router-dom"
-import App from "./App.jsx"
+import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import HomeScreen from "./screens/HomeScreen.jsx"
+import store from "./store"
+import { Provider } from "react-redux"
+import HomeScreen from "./screens/HomeScreen"
 import LoginScreen from "./screens/LoginScreen.jsx"
 import RegisterScreen from "./screens/RegisterScreen.jsx"
-import store from "./store.js"
-import PrivateRoute from "./components/PrivateRoute.jsx"
-import { Provider } from "react-redux"
 import ProfileScreen from "./screens/ProfileScreen.jsx"
+import PrivateRoute from "./components/PrivateRoute.jsx"
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
-			<Route index={true} path='/' element={<HomeScreen />}></Route>
-			<Route path='/login' element={<LoginScreen />}></Route>
-			<Route path='/register' element={<RegisterScreen />}></Route>
-			{/* private route */}
+			<Route index={true} path='/' element={<HomeScreen />} />
+			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
 			<Route path='' element={<PrivateRoute />}>
-				<Route path='/profile' element={<ProfileScreen />}></Route>
+				<Route path='/profile' element={<ProfileScreen />} />
 			</Route>
 		</Route>
 	)
 )
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
 		<React.StrictMode>
--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -1,11 +1,6 @@
 import Hero from "../components/Hero"
 
 const HomeScreen = () => {
-	return (
-		<div>
-			<Hero />
-		</div>
-	)
+	return <Hero />
 }
-
 export default HomeScreen
